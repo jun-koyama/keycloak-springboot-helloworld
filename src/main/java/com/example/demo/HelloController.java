bf9c7984012ce32f438920b93f9ed8089a83397a
jun-koyama <koyama@qahub.tv>
2018-06-03 22:37:02 +0900

    update

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +19,10 @@ public class HelloController {
 	
 //	@RequestMapping(value = "/hello")
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView hello(HttpSession ses, ModelAndView mav, Locale locale) {
+    public ModelAndView hello(KeycloakPrincipal<KeycloakSecurityContext> principal, HttpSession ses, ModelAndView mav, Locale locale) {
         mav.setViewName("hello");
+        AccessToken accessToken = principal.getKeycloakSecurityContext().getToken();
+        mav.addObject("name", accessToken.getFamilyName() + accessToken.getGivenName());
         mav.addObject("message", "Hello! Spring Boot and Keycloak!");
         return mav;
     }
